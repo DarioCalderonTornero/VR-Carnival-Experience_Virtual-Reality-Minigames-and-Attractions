1fa51b88e26a7ce4eb4ac68049a921e78eea534f
Alexr5k7 <152971030+Alexr5k7@users.noreply.github.com>
2025-03-11 16:47:19 +0100

    NoriaSpline

--- Assets/Scripts/Ferry wheel movement.cs
+++ Assets/Scripts/Ferry wheel movement.cs
@@ -8,6 +8,11 @@ public class Ferrywheelmovement : MonoBehaviour
 
     private bool canMoveCabin;
 
+    private void Awake()
+    {
+        StartCoroutine(MoveCabin());
+    }
+
     private void Update()
     {
         if (canMoveCabin)
@@ -22,18 +27,11 @@ public class Ferrywheelmovement : MonoBehaviour
        
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Player detected");
-            StartCoroutine(MoveCabin());
-        }
-    }
+    
 
     private IEnumerator MoveCabin()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(0f);
         canMoveCabin = true;
     }
 
