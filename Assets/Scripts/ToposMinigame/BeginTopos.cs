7aeef6abdc7871eb1e91de498aa1fcad368160cf
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2025-05-16 00:45:00 +0200

    Look Hammers 100%

--- Assets/Scripts/ToposMinigame/BeginTopos.cs
+++ Assets/Scripts/ToposMinigame/BeginTopos.cs
@@ -11,6 +11,10 @@ public class BeginTopos : MonoBehaviour
     [SerializeField] private GameObject[] hammerGameObjects;
     [SerializeField] private float delayBetweenHammers = 1f;
 
+    [SerializeField] private Camera cam; // La cámara del jugador (cabeza)
+    [SerializeField] private Transform targetTransform; // El transform al que la cámara debe mirar
+    [SerializeField] private Transform playerBody; // El cuerpo del jugador o XR Rig
+
     private void Awake()
     {
         Instance = this;
@@ -25,12 +29,14 @@ public class BeginTopos : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            RotatePlayerToTarget(); // Rota todo el XR Rig o jugador
             StartCoroutine(ShowHammersWithDelay());
         }
     }
 
     private IEnumerator ShowHammersWithDelay()
     {
+        yield return new WaitForSeconds(2f);
         foreach (var ham in hammerGameObjects)
         {
             ham.SetActive(true);
@@ -46,6 +52,26 @@ public class BeginTopos : MonoBehaviour
         }
 
         OnBeginTopos?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void RotatePlayerToTarget()
+    {
+        // Calcular la rotación deseada
+        Quaternion targetRotation = Quaternion.LookRotation(targetTransform.position - cam.transform.position);
+
+        // Rotar el objeto que contiene la cámara 
+        StartCoroutine(RotateOverTime(targetRotation));
+    }
+
+    private IEnumerator RotateOverTime(Quaternion targetRotation)
+    {
+        // Mientras la rotación no sea lo suficientemente cercana, sigue rotando
+        while (Quaternion.Angle(playerBody.rotation, targetRotation) > 0.1f)
+        {
+            // Realizar la interpolación suave para rotar lentamente
+            playerBody.rotation = Quaternion.Slerp(playerBody.rotation, targetRotation, Time.deltaTime * 2f);
+            yield return null;
+        }
     }
 
     private void HideHammers()
