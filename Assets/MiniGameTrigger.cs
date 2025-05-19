fe16ed6214b6e9329cf8c4eed55c9f81762aeab7
MRamon004 <marcosramon2004@gmail.com>
2025-05-19 23:09:20 +0200

    Globos en main

---
+++ Assets/MiniGameTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MinigameTrigger : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            GameObject generador = GameObject.Find("Generador de globos");
+
+            BalloonSpawner spawner = generador.GetComponent<BalloonSpawner>();
+
+            spawner.enabled = true;
+        }
+    }
+}
