512e399c5a1e9e47a16eed2c357ad6ab83a00df5
DarioCalderonTornero <152275116+DarioCalderonTornero@users.noreply.github.com>
2025-05-28 22:57:58 +0200

    Basket Minigame 40%

---
+++ Assets/Scripts/BasketMinigame/Basket Timer.cs
@@ -0,0 +1,54 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class BasketTimer : MonoBehaviour
+{
+
+    [SerializeField] private float currentTime;
+    [SerializeField] private float timerDuration = 60;
+    [SerializeField] private bool timerRunning = true;
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    public static event EventHandler OnBasketTimerFinish;
+
+    private void Start()
+    {
+        BasketManager.OnBasketMinigameStart += BasketManager_OnBasketMinigameStart;
+        //currentTime = timerDuration;
+    }
+
+    private void BasketManager_OnBasketMinigameStart(object sender, System.EventArgs e)
+    {
+        currentTime = timerDuration;
+        timerRunning = true;
+        Debug.Log("Start Basket timer");
+    }
+
+    private void Update()
+    {
+        if (!timerRunning)
+            return;
+
+        if (timerRunning)
+        {
+            currentTime -= Time.deltaTime;
+            Debug.Log(currentTime);
+
+            if (currentTime <= 0f)
+            {
+                Debug.Log("Basket Timer Finish");
+                OnBasketTimerFinish?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        string formattedTime = currentTime.ToString("00.00");
+        formattedTime = formattedTime.Replace(",", ":").Replace(".", ":"); 
+        timerText.text = formattedTime;
+    }
+
+    public float GetCurrentTime()
+    {
+        return currentTime;
+    }
+}
---
+++ Assets/Scripts/BasketMinigame/BasketManager.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class BasketManager : MonoBehaviour
+{
+    public static event EventHandler OnBasketMinigameStart;
+
+    private bool ballGrabbed = false;
+    private bool basketMinigameStarted = false;
+
+    private void Start()
+    {
+        //StartBasketMinigame();
+    }
+
+    public void StartBasketMinigame()
+    {
+        if (!ballGrabbed)
+        {
+            Debug.Log("StartBasketMinigame");
+            ballGrabbed = true;
+            basketMinigameStarted = true;
+            StartCoroutine(CountDown.Instance.Countdown(() =>
+            {
+                OnBasketMinigameStart?.Invoke(this, EventArgs.Empty);
+            }));
+        }
+    }
+}
